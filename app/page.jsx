7e255a7ad34d739dd7bcb7193c52773aaf5aa74c
Eliezer Nsengi <iamnsengi@icloud.com>
2022-01-13 17:58:07 +0200

    ft: test using Link to move through pages

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="text-center">
       <p>Hello world</p>
+      <p>
+        <Link href="/about">About Us</Link>{" "}
+      </p>
     </div>
   );
 }
