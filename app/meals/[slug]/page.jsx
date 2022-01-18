46d5fd27a14ddedfc7791eff03950f13a178e12b
Eliezer Nsengi <iamnsengi@icloud.com>
2022-01-18 19:35:55 +0200

    ft: add corresponing sample pages

---
+++ app/meals/[slug]/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Meal = () => {
+  return <div>Individual meal page</div>;
+};
+
+export default Meal;
---
+++ app/meals/share/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShareMeal = () => {
+  return <div>Meals sharing page</div>;
+};
+
+export default ShareMeal;
