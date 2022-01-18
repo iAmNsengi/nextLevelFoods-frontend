4b542d5e6c20e903a862c210335d31ac0afc2d58
Eliezer Nsengi <iamnsengi@icloud.com>
2022-01-18 19:49:26 +0200

    ft: test route layout

---
+++ app/meals/layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MealsLayout = ({ children }) => {
+  return (
+    <div>
+      <p>Navigation</p>
+      {children}
+    </div>
+  );
+};
+
+export default MealsLayout;
