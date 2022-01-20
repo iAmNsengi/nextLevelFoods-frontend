3bf58a4d04b327a14fad0b2c551551e29d9efc17
Eliezer Nsengi <iamnsengi@icloud.com>
2022-01-20 20:24:55 +0200

    ft: add navigation

--- components/main-header.jsx
+++ components/main-header.jsx
@@ -8,6 +8,16 @@ const MainHeader = () => {
       <Link href={"/"}>
         <img src={logoImg.src} alt="A plate with food on iit" /> Next Level Food
       </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/meals"}>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href={"/community"}>Community</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
