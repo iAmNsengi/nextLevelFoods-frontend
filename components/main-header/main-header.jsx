4837a507187290ee59e3879a7238532d99f7a60c
Eliezer Nsengi <iamnsengi@icloud.com>
2022-01-29 09:49:39 +0200

    update: highlight active link from pathname

--- components/main-header/main-header.jsx
+++ components/main-header/main-header.jsx
@@ -1,10 +1,13 @@
+"use client";
 import React from "react";
 import logoImg from "@/assets/logo.png";
 import Link from "next/link";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const MainHeader = () => {
+  const pathName = usePathname();
   return (
     <header className={classes.header}>
       <Link href={"/"} className={classes.logo}>
@@ -14,10 +17,22 @@ const MainHeader = () => {
       <nav className={classes.nav}>
         <ul>
           <li>
-            <Link href={"/meals"}>Browse Meals</Link>
+            <Link
+              href={"/meals"}
+              className={
+                pathName.startsWith("/meals") ? classes.active : undefined
+              }
+            >
+              Browse Meals
+            </Link>
           </li>
           <li>
-            <Link href={"/community"}>Community</Link>
+            <Link
+              href={"/community"}
+              className={pathName === "/community" ? classes.active : undefined}
+            >
+              Community
+            </Link>
           </li>
         </ul>
       </nav>
