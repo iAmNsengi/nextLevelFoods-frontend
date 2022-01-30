c7e1edd8a7c670199083659b40ed58747be355a3
Eliezer Nsengi <iamnsengi@icloud.com>
2022-01-30 10:08:20 +0200

    add: meals UI update

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,9 +1,25 @@
-import Link from "next/link";
 import React from "react";
-import logoImg from "@/assets/logo.png";
+import classes from "./page.module.css";
+import Link from "next/link";
 
 const Meals = () => {
-  return <p>Meals page</p>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, create{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share Your Favourite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
+    </>
+  );
 };
 
 export default Meals;
