eed4bf0a58db07da717af26b74e78017b07cf87a
Eliezer Nsengi <iamnsengi@icloud.com>
2022-02-01 10:20:21 +0200

    render meals in the meals grid

--- components/meals/meals-grid.jsx
+++ components/meals/meals-grid.jsx
@@ -1,10 +1,13 @@
+import MealItem from "./meal-item";
 import classes from "./meals-grid.module.css";
 
 const MealsGrid = ({ meals }) => {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
-        <li id={meal.id}></li>
+        <li id={meal.id}>
+          <MealItem {...meal} />
+        </li>
       ))}
     </ul>
   );
