802289dee900b5026bfd0f12372c18009a3d640b
Eliezer Nsengi <iamnsengi@icloud.com>
2022-02-04 10:50:08 +0200

    refactor: return selection && render meals in the grid

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
 
 const Meals = async () => {
-  
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +22,7 @@ const Meals = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
