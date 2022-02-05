b109598b8586a68dab8ec4e5d9008ded7ac025e3
Eliezer Nsengi <iamnsengi@icloud.com>
2022-02-05 10:57:45 +0200

    fx: export loader

--- app/meals/loading.jsx
+++ app/meals/loading.jsx
@@ -1,3 +1,6 @@
+import classes from "./loading.module.css";
+
 const MealsLoadingPage = () => {
-  return <p>Fetching meals for you...</p>;
+  return <p className={classes.loading}>Fetching meals for you...</p>;
 };
+export default MealsLoadingPage;
