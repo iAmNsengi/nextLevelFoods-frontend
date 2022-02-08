36964c01c63630e6d815fd8c4f995d7446bcabaf
Eliezer Nsengi <iamnsengi@icloud.com>
2022-02-08 11:04:51 +0200

    ft: use Suspense to render loading meals

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
 import classes from "./page.module.css";
 import Link from "next/link";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import MealsLoadingPage from "./loading-out";
 
 const Meals = async () => {
   const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+};
 
+const MealsPage = async () => {
   return (
     <>
       <header className={classes.header}>
@@ -22,10 +26,12 @@ const Meals = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense fallback={<MealsLoadingPage />}>
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
 };
 
-export default Meals;
+export default MealsPage;
