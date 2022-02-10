64609b0dbb30c14af4866b570076b89a82a4851e
Eliezer Nsengi <iamnsengi@icloud.com>
2022-02-10 18:29:48 +0200

    ft: render single meals recipes

---
+++ app/meals/[mealSlug]/page.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import classes from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+
+const Meal = async ({ params }) => {
+  const meal = await getMeal(params.mealSlug);
+  console.log(meal);
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill />
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title} </h1>
+          <p className={classes.creator}>
+            {" "}
+            by <a href={`mailto: ${"EMAIL"}`}>{meal.creator} </a>
+          </p>
+          <p className={classes.summary}>{meal.summary} </p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
+};
+
+export default Meal;
