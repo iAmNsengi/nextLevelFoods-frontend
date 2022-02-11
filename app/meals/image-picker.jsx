a02fdc3250522b01b6822b20704ea0e4c73555a4
Eliezer Nsengi <iamnsengi@icloud.com>
2022-02-11 19:45:10 +0200

    ft: add custom button to pick images

--- app/meals/image-picker.jsx
+++ app/meals/image-picker.jsx
@@ -1,16 +1,33 @@
+"use client";
+import { useRef } from "react";
 import classes from "./image-picker.module.css";
 
 const ImagePicker = ({ label, name }) => {
+  const inputRef = useRef();
+
+  const handlePickClick = () => {
+    inputRef.current.click();
+  };
+
   return (
-    <div>
+    <div className={classes.picker}>
       <label htmlFor="image">{label}</label>
       <div className={classes.controls}>
         <input
+          ref={inputRef}
           type="file"
+          className={classes.input}
           id="image"
           accept="image/png, image/jpeg"
           name={name}
         />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickClick}
+        >
+          Pick an Image
+        </button>
       </div>
     </div>
   );
